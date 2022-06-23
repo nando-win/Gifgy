95cf8c3f47c6f1114bd10696359423a276421aa1
Fernando Winston <fernando.wcb@gmail.com>
2022-06-23 14:21:40 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Gifgy
+ Application to share gifs with other platforms and social media easily
---
+++ android/app/src/main/kotlin/com/gifgy/gifgy/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.gifgy.gifgy
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
